c5ec311559a5e5fb7fd995351a02f846a451378d
NessEFC <NessEFC@gmail.com>
2017-07-22 22:53:08 -0600

    add additional login sad-path test for missing email field

--- spec/features/user_can_login_spec.rb
+++ spec/features/user_can_login_spec.rb
@@ -18,7 +18,7 @@ RSpec.feature('Login') do
       expect(page).to have_link("Logout")
     end
 
-    scenario('cannot login successfully') do
+    scenario('cannot login successfully with missing password') do
       user = create(:user)
 
       visit root_path
@@ -26,6 +26,20 @@ RSpec.feature('Login') do
       expect(current_path).to eq(login_path)
 
       fill_in("session[email]", with: user.email)
+      click_on('Login')
+
+      expect(current_path).to eq(login_path)
+      expect(page).to have_content("Login failed, make sure your email and password are correct.")
+    end
+
+    scenario('cannot login successfully with missing email') do
+      user = create(:user)
+
+      visit root_path
+
+      expect(current_path).to eq(login_path)
+
+      fill_in("session[password]", with: user.password)
       click_on('Login')
 
       expect(current_path).to eq(login_path)
