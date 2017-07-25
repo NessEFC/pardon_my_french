3ef78e344d5b62fbc05add78f9d88694c47d422c
NessEFC <NessEFC@gmail.com>
2017-07-24 22:31:05 -0600

    refactor functions out of index.js to card.js

--- app/assets/javascripts/card.js
+++ app/assets/javascripts/card.js
@@ -5,6 +5,30 @@ class Card {
     this.user_id = card.user_id
   }
 
+  static createCard(e) {
+    e.preventDefault()
+
+    const word = $('input[name="card[french_word]"]').val().toLowerCase()
+
+    $.ajax({
+      type: 'POST',
+      url: '/api/v1/cards',
+      dataType: 'json',
+      data: {
+        card:
+          {
+            french_word: word
+          }
+      },
+      success: function(data) {
+        const card = new Card(data.card)
+        card.appendToPage()
+        $('input[name="card[french_word]"]').val('')
+      },
+      error: function(data) {}
+    })
+  }
+
   appendToPage() {
     $('.new-card').append(`
       <div class="word">
--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,27 +1,3 @@
 $(() => {
-  $('#create-card-btn').on('click', createCard)
+  $('#create-card-btn').on('click', Card.createCard)
 })
-
-const createCard = (e) => {
-  e.preventDefault()
-
-  const word = $('input[name="card[french_word]"]').val().toLowerCase()
-
-  $.ajax({
-    type: 'POST',
-    url: '/api/v1/cards',
-    dataType: 'json',
-    data: {
-      card:
-        {
-          french_word: word
-        }
-    },
-    success: function(data) {
-      const card = new Card(data.card)
-      card.appendToPage()
-      $('input[name="card[french_word]"]').val('')
-    },
-    error: function(data) {}
-  })
-}
