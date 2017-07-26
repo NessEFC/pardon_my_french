f19147c926d185efc52e62f22ebbdb60d31f58c6
NessEFC <NessEFC@gmail.com>
2017-07-25 23:43:10 -0600

    add deck references to cards table and run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170726053546) do
+ActiveRecord::Schema.define(version: 20170726054210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20170726053546) do
     t.datetime "updated_at", null: false
     t.text "personal_connection"
     t.string "english_word"
+    t.bigint "deck_id"
+    t.index ["deck_id"], name: "index_cards_on_deck_id"
     t.index ["user_id"], name: "index_cards_on_user_id"
   end
 
@@ -41,5 +43,6 @@ ActiveRecord::Schema.define(version: 20170726053546) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "cards", "decks"
   add_foreign_key "cards", "users"
 end
