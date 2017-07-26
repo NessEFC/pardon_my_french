cf2a8c63d29dafcc8a1601fd1ef02c078d31348c
NessEFC <NessEFC@gmail.com>
2017-07-25 19:48:14 -0600

    add event listener in document ready function to the keep button

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -2,4 +2,5 @@ $(() => {
   $('#create-card-btn').on('click', Card.createCard)
   $('.creation-mode-container').on('blur', '.new-card p', Card.update)
   $('.creation-mode-container').on('click', '#discard-btn', Card.delete)
+  $('.creation-mode-container').on('click', '#keep-btn', Card.keep)
 })
