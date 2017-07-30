27e124f84829151ca20adfd3507ec9b928be1f3a
NessEFC <NessEFC@gmail.com>
2017-07-30 16:31:19 -0600

    clear flash messages for sessions and registrations without refresh

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -3,4 +3,5 @@ $(() => {
   $('.creation-mode-container').on('blur', '.new-card p', Card.update)
   $('.creation-mode-container').on('click', '#discard-btn', Card.delete)
   $('.creation-mode-container').on('click', '#keep-btn', Card.keep)
+  Card.clearAlert()
 })
